96a8b937375d8c62cb558e10f88a8d31bf4029ca
Aihua Chen <43660978+Aihua123@users.noreply.github.com>
2019-03-17 12:39:25 -0600

    Add files via upload

---
+++ Python Challenges/Maximum path sum I.py
@@ -0,0 +1,32 @@
+# python challenge: "https://projecteuler.net/problem=18"
+
+data = """75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
+
+
+lines = [ list(map(int, x.split())) for x in data.split("\n") if x]
+
+# row will go from 2 to 1 to 0
+for row in range(len(lines)- 2,-1,-1):
+    # never go past the last column value in the particular row
+    for col in range(0,len(lines[row])):
+                                  # down move       # down right move
+        lines[row][col] += max([lines[row+1][col], lines[row+1][col+1]])
+print(lines)
+
+# lines 
+# >> [[23], [20, 19], [10, 13, 15], [8, 5, 9, 3]]
+# The top value in the array is 1074, which represents the best possible sum
